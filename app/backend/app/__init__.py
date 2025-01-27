4f83ca55b59bd13c781b45979ddcbe0f0e3c0409
ryusukeyasiro <ryusukeyasiro@gmail.com>
2025-01-28 03:49:50 +0900

    __init__の設定 ここに設定を書いていく

---
+++ app/backend/app/__init__.py
@@ -0,0 +1,33 @@
+# __init__.pyファイルの最も基本的な役割は、そのディレクトリをPythonのパッケージとして認識させることです。
+# これにより、他のモジュールからこのディレクトリ内のファイルをインポートすることが可能になります。
+
+# アプリケーションオブジェクトとデータベースの紐付け、config定義、ログインマネージャーを紐付ける
+# Migrateは、データベースをマイグレーション（データ移行）する機能です。
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+class Base(DeclarativeBase):
+    pass
+# flaskインスタンス生成
+app = Flask(__name__)
+
+# corsの設定
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# SQLAlchemyのイベントシステムを無効化します。
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# セッションやその他のセキュリティ機能で使用される秘密鍵を設定
+app.config['SECRET_KEY'] = 'your-secret-key'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
